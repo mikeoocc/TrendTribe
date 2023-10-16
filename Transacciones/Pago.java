774c35629e3516dba566ea05b7aa091290d6ac56
Javotass <145339076+Javotass@users.noreply.github.com>
2023-10-16 18:35:39 +0200

    Create Pago.java

---
+++ Transacciones/Pago.java
@@ -0,0 +1,14 @@
+package Transacciones;
+
+public class Pago {
+	
+	private String idPago;
+	private String fechaDePago;
+	private String metodoDePago;
+	
+	/*
+	 * public void cancelarPago();
+	 * public void generarRecibo();
+	 */
+
+}
